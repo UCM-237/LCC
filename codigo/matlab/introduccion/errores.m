9f4a03ab55fdf9402272f903489364da6e81c5ec
juanjimenez <juan.jimenez@fis.ucm.es>
2021-03-06 19:48:59 +0100

    nuevos ficheros de codigo del tema 1

---
+++ codigo/matlab/introduccion/errores.m
@@ -0,0 +1,6 @@
+
+%script con errores,
+y=[1 2 3; 4 5 6; 2 3 7] %a esta matriz le falta un elemento en la última fila
+x=[1 2 3; 4 5 6]
+z=y*x %las matrices no pueden multiplicarse entre si por que no coinciden
+      %numero de columnas de la primera con numero de filas de la segunda
